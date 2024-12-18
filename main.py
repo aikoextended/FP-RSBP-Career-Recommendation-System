fe6c926f2ff08029a0efe8af8947ea7f0b9d3afc
Decya Giovanni <144076663+dgio26@users.noreply.github.com>
2024-12-18 14:24:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def load_and_prepare_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Train K-Nearest Neighbors Classifier
-    k = 5
+    k = 3
     model = KNeighborsClassifier(n_neighbors=k)
     model.fit(X_train, y_train)
 
